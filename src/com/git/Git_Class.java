0d1d7bc50a65bab9ea629fd4f12f4a46efe1ae3a
maran999 <manimaran.gopinathan@gmail.com>
2021-10-23 20:05:30 +0530

    New

--- src/com/git/Git_Class.java
+++ src/com/git/Git_Class.java
@@ -3,10 +3,12 @@ package com.git;
 public class Git_Class {
 
 	public static void main(String[] args) {
-		
-		int a =10;
+
+		int a = 10;
+		int ca = 50;
 		int b = 20;
-		int ab=30;
+		int cd = 40;
+		int ab = 30;
 
 	}
 
